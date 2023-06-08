0b46ebc35dc94142660550f7eca694c4b32b02c1
Pioupia <pioupiadeux@gmail.com>
2023-06-08 21:47:38 +0200

    Feat (WebSocket): create the camera rotation for each player

--- src/Managers/Websocket.ts
+++ src/Managers/Websocket.ts
@@ -1,10 +1,21 @@
+import {PerspectiveCamera} from "three";
+
+enum RESPONSE_CODE {
+    close_connection,
+    first,
+    second
+}
+
+
 export default class Socket extends WebSocket {
     location: string;
+    camera: PerspectiveCamera;
 
-    constructor(location: string) {
+    constructor(location: string, camera: PerspectiveCamera) {
         super(location);
 
         this.location = location;
+        this.camera = camera;
 
         this.onerror = this.onError.bind(this);
         this.onclose = this.onClose.bind(this);
@@ -12,14 +23,20 @@ export default class Socket extends WebSocket {
     }
 
     private onClose() {
-        new Socket(this.location)
+        new Socket(this.location, this.camera);
     }
 
     private onError(err: Event) {
         console.log("[WebSocket] Got an error:", err);
     }
 
-    private onMessage(ev: Event) {
-        console.log(ev);
+    private onMessage(ev: MessageEvent) {
+        const type = RESPONSE_CODE[+ev.data];
+
+        switch (type) {
+            case 'second': {
+                this.camera.rotateZ(Math.PI);
+            }
+        }
     }
 }
