41cca14ed5c90a7df86f36f6a625889c99552f1e
Pioupia <pioupiadeux@gmail.com>
2023-06-09 18:45:39 +0200

    Feat (Pieces): add the selected color

--- src/Managers/Pieces.ts
+++ src/Managers/Pieces.ts
@@ -5,7 +5,9 @@ export default class Pieces {
     x: number;
     y: number;
 
+    defaultColor: number;
     color: number;
+
     scene: Mesh;
 
     loader: Loaders;
@@ -14,18 +16,45 @@ export default class Pieces {
 
     size: number;
 
+    selected: boolean;
+
     constructor(x: number, y: number, color: 0 | 1, checkboard: Mesh, loader: Loaders, size?: number) {
         this.x = x;
         this.y = y;
 
-        this.color = color ? 0xdcdeaf : 0x424242;
+        this.defaultColor = color ? 0xdcdeaf : 0x424242;
+        this.color = this.defaultColor;
         this.scene = checkboard;
 
         this.loader = loader;
 
         this.size = size || 1;
 
+        this.selected = false;
+
         loader.loadObject('/pieces.glb', gltf => this.onLoaded(gltf));
+    }
+
+    private adaptColor() {
+        if (this.selected) {
+            this.color = 0xff0000;
+        } else {
+            this.color = this.defaultColor;
+        }
+    }
+
+    public toggleSelected() {
+        this.selected = !this.selected;
+        this.adaptColor();
+
+        return this;
+    }
+
+    public setSelected(value: boolean) {
+        this.selected = value;
+        this.adaptColor();
+
+        return this;
     }
 
     private async onLoaded(obj: Group) {
